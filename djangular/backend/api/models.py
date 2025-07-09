c715376573574ce03f77fe05669c39d72df0836c
F4brici0L4yme <rlaymes@unsa.edu.pe>
2025-07-09 11:27:14 -0500

    agrega corsheaders a installedApps - MovieViewSet creado - actualiza urls para registrar movie db al frontend

--- djangular/backend/api/models.py
+++ djangular/backend/api/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Movie(models.Model):
     title = models.CharField(max_length=32)
-    desc = models.CharField(max_length=256)
+    desc = models.CharField(max_length=256, default="-")
     year = models.IntegerField()
